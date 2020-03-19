066de701c5ee43d2a04e7b8a43723608bdac6f97
mike <jmj00@ucsb.edu>
2020-03-19 11:14:50 -0700

    add messages for error catching

--- R/getRawData.R
+++ R/getRawData.R
@@ -27,40 +27,49 @@ getRawData = function(AOI, dir, name){
     if(!dir.exists(name.dir)){ dir.create(name.dir)}
     # Write AOI
     aoi.path = paste0(name.dir, "//", name, ".shp")
-    AOI = AOI %>% st_transform(3857)
+    AOI      = AOI %>%
+      st_transform(3857)
     sf::write_sf(AOI, dsn = aoi.path)
 
     # Build list
     to_process = data.frame(HUC6 = rep(HUC6, each = 2),
                             name = name,
-                            raw.files = grep("tif", list.files(dir, pattern = paste(HUC6, collapse = "|"), full.names = TRUE), value = TRUE),
+                            raw.files = grep("tif",
+                                             list.files(dir, pattern = paste(HUC6, collapse = "|"),
+                                             full.names = TRUE), value = TRUE),
                             stringsAsFactors = FALSE) %>%
       mutate(cropped = paste0(name.dir, "/", basename(raw.files))) %>%
-      mutate( type = ifelse(grepl("hand", raw.files), "HAND", "CATCH")) %>%
-      mutate(method = ifelse(type == "HAND", 'bilinear', 'near'))
+      mutate(type    = ifelse(grepl("hand", raw.files), "HAND", "CATCH")) %>%
+      mutate(method  = ifelse(type == "HAND", 'bilinear', 'near'))
 
     # Crop
-    for(i in 1:nrow(to_process)){
-      crop_project(to_process$raw.files[i], to_process$cropped[i],
-                   to_process$name[i], aoi.path, to_process$method[i])
-    }
+    message('Cropping and Projecting Rasters...')
+      for(i in 1:nrow(to_process)){
+        crop_project(to_process$raw.files[i], to_process$cropped[i],
+                     to_process$name[i], aoi.path, to_process$method[i])
+      }
 
     # Align
-    for(i in 1:length(HUC6)){ align_rasters(HUC6[i], name.dir) }
+    message('Aligning Rasters...')
+      for(i in 1:length(HUC6)){
+        align_rasters(HUC6[i], name.dir)
+      }
 
     # Merge Rasters
-    files = merge_rasters(name.dir, name)
-
+    message('Merging Rasters...')
+    files  = merge_rasters(name.dir, name)
     comids = unique(getValues(raster(files$catch.path)))
 
     } else {
-      files = list(hand.path = hand.path, catch.path = catch.path)
+      message('Files Already Processed...')
+      files = list(hand.path = hand.path,
+                   catch.path = catch.path)
     }
 
     files$rating.path =  paste0(name.dir, '/rating_',name,'.fst')
 
     if(!file.exists(files$rating.path)){
-
+    message('Subsetting Rating Curve Files...')
     lapply(HUC6, get_rc_table, raw.dir = dir, comids = comids ) %>%
       bind_rows() %>%
       fst::write.fst(path = files$rating.path)
